a8a41832b4c369fc5816df0144991cb9b8aec308
willyspa <armed95@hotmail.fr>
2018-12-23 03:29:27 +0100

    ajout des deux derniers exos

---
+++ exo_08.rb
@@ -0,0 +1,3 @@
+puts "quel est ton prénom ?"
+user_name = gets.chomp
+puts "Bonjour, #{user_name} !"
---
+++ exo_09.rb
@@ -0,0 +1,6 @@
+puts "quel est ton prénom ?"
+user_first_name = gets.chomp
+puts "quel est ton nom ?"
+user_last_name = gets.chomp
+
+puts "Bonjour, #{user_first_name} #{user_last_name}!"
